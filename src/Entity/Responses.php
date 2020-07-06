64b8847dd18c2aba1c5f8a5c47bb5eb618eadee0
Jeremy <jeremy.schiappapietre@hetic.net>
2020-07-06 11:13:46 +0200

    [BACK] First Push

---
+++ src/Entity/Responses.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResponsesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ResponsesRepository::class)
+ */
+class Responses
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $response;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResponse(): ?string
+    {
+        return $this->response;
+    }
+
+    public function setResponse(string $response): self
+    {
+        $this->response = $response;
+
+        return $this;
+    }
+}
